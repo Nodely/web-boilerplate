40946ae876e08e130bf09b3d695f5a721840426a
Ivan Solovyev <ivan.soloviev@gmail.com>
2019-11-04 22:21:44 -0800

    Added build to repository

---
+++ build/precache-manifest.294d77b60e7443fc47ec1cb9140bd859.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e429bc28b6d5b2e46bb6",
+    "url": "/static/css/main.1e409f38.chunk.css"
+  },
+  {
+    "revision": "e429bc28b6d5b2e46bb6",
+    "url": "/static/js/main.7d942af5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "50b12c72df6e5645b72e",
+    "url": "/static/js/2.62ea35af.chunk.js"
+  },
+  {
+    "revision": "fa3f39fe060207d7f0e41496dc7c84d8",
+    "url": "/index.html"
+  }
+];
